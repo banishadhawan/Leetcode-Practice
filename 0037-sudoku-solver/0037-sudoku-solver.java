5742ec22796bf206045bf6653301557e5e1f068c
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-03-26 23:18:38 +0530

    Time: 381 ms (19.65%), Space: 42.7 MB (90.4%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board)) return true;
+
+                            board[i][j] = '.'; // backtrack
+                        }
+                    }
+                    return false; // no valid number
+                }
+            }
+        }
+        return true; // solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // row check
+            if (board[row][i] == c) return false;
+
+            // column check
+            if (board[i][col] == c) return false;
+
+            // 3x3 grid check
+            int r = 3 * (row / 3) + i / 3;
+            int cl = 3 * (col / 3) + i % 3;
+
+            if (board[r][cl] == c) return false;
+        }
+        return true;
+    }
+}
