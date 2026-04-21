5ea2aae224aecd22c5f1650bdf687edbe9c88f72
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-04-21 12:14:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,30 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int j=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=0){
+//                 //swap(nums[i],nums[k])
+//                 int temp=nums[j];
+//                 nums[j]=nums[i];
+//                 nums[i]=temp;
+//                 j++;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                //swap(nums[i],nums[k])
-                int temp=nums[j];
-                nums[j]=nums[i];
-                nums[i]=temp;
-                j++;
+        int i=0;
+
+        for(int num:nums){
+            if(num!=0){
+                nums[i++]=num;
             }
+        }
+        while(i<nums.length){
+            nums[i]=0;
+            i++;
         }
     }
 }
