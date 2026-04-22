f8ed7ca730a760137bc3e0cf27ea8ec3a79c4d1b
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-04-22 12:37:45 +0530

    Time: 0 ms (100%), Space: 43.5 MB (93.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -6,8 +6,8 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int temp=matrix[i][j];
-                matrix[i][j] =matrix[j][i];
-                matrix[j][i] = temp;
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
 
@@ -15,12 +15,12 @@ class Solution {
             int left=0, right=n-1;
             while(left<right){
                 int temp=matrix[i][left];
-                matrix[i][left] =matrix[i][right];
+                matrix[i][left]=matrix[i][right];
                 matrix[i][right]=temp;
-                left++ ;right--;
+                left++; right--;
             }
         }
-
-
     }
 }
+
+       
