76b0218a7a22a1a095f0119471363757c6cc1def
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-05-10 22:58:08 +0530

    Time: 3 ms (88.45%), Space: 48.1 MB (64.05%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -4,11 +4,10 @@ class Solution {
         int n=matrix[0].length;
 
         int row=0, col=n-1;
-        while(row<m && col>=0 ){
+        while(row<m && col>=0){
             if(matrix[row][col]==target) return true;
             else if(matrix[row][col]<target) row++;
             else col--;
-
         }
         return false;
     }
