29d11211924998a7b8dd9338893f1eb4ea6c5961
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-06-17 12:53:03 +0530

    Time: 361 ms (76.14%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-SELECT
-    query_name,
-    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
-    ROUND(
-        AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100,
-        2
-    ) AS poor_query_percentage
-FROM Queries
-GROUP BY query_name;
+select query_name, ROUND(AVG(rating/position),2) AS quality,
+ROUND(AVG(IF(rating < 3 ,1,0))*100,2) AS poor_query_percentage
+from Queries
+group by query_name
