8e97a56b2182c5ae394f05e018e9ac12203b199d
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-06-28 19:21:51 +0530

    Time: 1 ms (99.89%), Space: 46.4 MB (81.7%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-       List<List<String>> ans = new ArrayList<>();
+      
+      List<List<String>> ans = new ArrayList<>();
 
-        char[][] board = new char[n][n];
-        for(char[] row:board){
-            Arrays.fill(row,'.');
-        }
+      char[][] board = new char[n][n];
 
-        boolean[] leftrow = new boolean[n];
-        boolean[] lowerdiag = new boolean[2*n-1];
-        boolean[] upperdiag = new boolean[2*n-1];
+      for(char[] r : board){
+        Arrays.fill(r,'.');
+      }
 
-        backtrack(0,board,leftrow,lowerdiag,upperdiag, ans,n);
+      boolean[] leftrow = new boolean[n];
+      boolean[] lowerdiag = new boolean[2*n-1];
+      boolean[] upperdiag = new boolean[2*n-1];
 
-        return ans;
+    
+    backtrack(0,board, ans, leftrow, lowerdiag, upperdiag, n);
+
+    return ans;
     }
 
-    private void backtrack(int row, char[][] board, boolean[] leftrow, boolean[] lowerdiag, boolean[] upperdiag, List<List<String>> ans, int n){
-        if(row==n){
+    private void backtrack(int row, char[][] board, List<List<String>> ans, boolean[] leftrow, boolean[] lowerdiag, boolean[] upperdiag,int n){
+
+        if(row == n){
             List<String> temp = new ArrayList<>();
 
-            for(char[] r:board){
+            for(char[] r : board){
                 temp.add(new String(r));
             }
 
@@ -29,26 +33,23 @@ class Solution {
         }
 
         for(int col=0;col<n;col++){
+        if(!leftrow[col] && !lowerdiag[row+col] && !upperdiag[n-1 + col-row]){
 
-            if(!leftrow[col] && !lowerdiag[row+col] && !upperdiag[n-1 + col-row]){
+            board[row][col]='Q';
 
-                board[row][col] = 'Q';
+            leftrow[col]=true;
+            lowerdiag[row+col]=true;
+            upperdiag[n-1 + col-row]=true;
 
-                leftrow[col]=true;
-                lowerdiag[row+col]=true;
-                upperdiag[n-1+col-row]=true;
+            backtrack(row+1, board, ans, leftrow, lowerdiag, upperdiag,n);
 
-                backtrack(row+1,board,leftrow,lowerdiag,upperdiag,ans,n);
+            board[row][col] = '.';
 
-                board[row][col]='.';
-
-                leftrow[col]=false;
-                lowerdiag[row+col]=false;
-                upperdiag[n-1+col-row]=false;
-            }
+            leftrow[col]=false;
+            lowerdiag[row+col]=false;
+            upperdiag[n-1 + col-row]=false;
         }
-
+        }
     }
 }
-
-   
+ 
