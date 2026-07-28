87219128d0c0b5db2577044baead111351a53338
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-07-28 16:37:02 +0530

    Time: 12 ms (46.95%), Space: 47.3 MB (22.57%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public String predictPartyVictory(String senate) {
+
+        Queue<Integer> radiant = new LinkedList<>();
+        Queue<Integer> dire = new LinkedList<>();
+
+        int n = senate.length();
+
+        // Store indices
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                radiant.offer(i);
+            } else {
+                dire.offer(i);
+            }
+        }
+
+        // Simulate rounds
+        while (!radiant.isEmpty() && !dire.isEmpty()) {
+
+            int r = radiant.poll();
+            int d = dire.poll();
+
+            if (r < d) {
+                // Radiant acts first
+                radiant.offer(r + n);
+            } else {
+                // Dire acts first
+                dire.offer(d + n);
+            }
+        }
+
+        return radiant.isEmpty() ? "Dire" : "Radiant";
+    }
+}
